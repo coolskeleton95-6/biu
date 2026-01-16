c97613e10872662dd3169af32919714c3281f63a
Jeremy <ggajy123@gmail.com>
2026-01-15 17:28:48 -0800

    fix: door detector precision fix

--- scripts/actors/door_detector.gd
+++ scripts/actors/door_detector.gd
@@ -3,7 +3,6 @@ class_name DoorDetector
 
 signal state_changed(is_active: bool)
 
-var active_bodies: Array[Node2D] = []
 var is_active: bool = false
 
 func _ready() -> void:
@@ -11,28 +10,22 @@ func _ready() -> void:
 	monitoring = true
 	monitorable = false
 	
-	# Connect collision signals
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-	
 	# Visual feedback (Start inactive)
-	modulate = Color(0.5, 1.5, 0.5)
+	modulate = Color.WHITE
 
-func _on_body_entered(body: Node2D) -> void:
-	# Checks for anything in the "box" group (Box.gd and Bomb.gd both have this)
-	print_debug(body.name)
-	if body.is_in_group("box"):
-		active_bodies.append(body)
-		_update_state()
-
-func _on_body_exited(body: Node2D) -> void:
-	if body in active_bodies:
-		active_bodies.erase(body)
-		_update_state()
-
-func _update_state() -> void:
-	var new_state = active_bodies.size() > 0
+func _physics_process(_delta: float) -> void:
+	var bodies = get_overlapping_bodies()
+	var found_valid_body = false
 	
+	for body in bodies:
+		if body.is_in_group("box"):
+			if global_position.distance_to(body.global_position) <= 8.0:
+				found_valid_body = true
+				break
+	
+	_update_state(found_valid_body)
+
+func _update_state(new_state: bool) -> void:
 	if is_active != new_state:
 		is_active = new_state
 		state_changed.emit(is_active)
@@ -41,4 +34,4 @@ func _update_state() -> void:
 		if is_active:
 			modulate = Color(0.5, 1.5, 0.5) # Glow Green
 		else:
-			modulate = Color(0.5, 1.5, 0.5)
+			modulate = Color.WHITE
